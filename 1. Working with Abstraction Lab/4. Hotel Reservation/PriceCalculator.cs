645e2374b9acdc9af7dfbed3891a5d2ffbc26e2b
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-03 18:00:28 +0200

    CSharp

---
+++ 1. Working with Abstraction Lab/4. Hotel Reservation/PriceCalculator.cs
@@ -0,0 +1,36 @@
+﻿namespace HotelReservation
+{
+    using System;
+    public class PriceCalculator
+    {
+        private decimal pricePerDay;
+        private int numberOfDays;
+        private Season season;
+        private Discount discountType;
+
+        public PriceCalculator(string[] commandParts)
+        {
+            this.pricePerDay = decimal.Parse(commandParts[0]);
+            this.numberOfDays = int.Parse(commandParts[1]);
+            this.season = Enum.Parse<Season>(commandParts[2]);
+            this.discountType = Discount.None;
+
+            if (commandParts.Length == 4)
+            {
+                this.discountType = Enum.Parse<Discount>(commandParts[3]);
+            }
+        }
+
+        public decimal CalculatePrice()
+        {
+            var multiplier = (int)this.season;
+            var discountMultiplier = (decimal)this.discountType / 100;
+            var priceBeforeDiscount = this.numberOfDays * this.pricePerDay * multiplier;
+            var discountedAmount = priceBeforeDiscount * discountMultiplier;
+
+            var finalPrice = priceBeforeDiscount - discountedAmount;
+
+            return finalPrice;
+        }
+    }
+}
