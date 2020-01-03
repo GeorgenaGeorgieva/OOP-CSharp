50f911823866d6ddd0f1c19a5a4de26d7f5380cc
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-03 17:53:48 +0200

    CSharp

---
+++ 1. Working with Abstraction Lab/1. Rhombus of Stars/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace RhombusOfStars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int figureSize = int.Parse(Console.ReadLine());
+
+            for (int row = 1; row <= figureSize; row++)
+            {
+                PrintRow(figureSize, row);
+            }
+
+            for (int row = figureSize - 1; row >= 1; row--)
+            {
+                PrintRow(figureSize, row);
+            }
+        }
+
+        private static void PrintRow(int figureSize, int row)
+        {
+            for (int col = 0; col < figureSize - row; col++)
+            {
+                Console.Write(" ");
+            }
+
+            Console.Write("*");
+
+            for (int col = 1; col < row; col++)
+            {
+                Console.Write(" *");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
