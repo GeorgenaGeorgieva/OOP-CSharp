bf96da2d719c9d2a552cc40d859e549d87d6f352
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-03 17:55:56 +0200

    CSharp

---
+++ 1. Working with Abstraction Lab/2. Point in Rectangle/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace PointInRectangle
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] coordinates = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int topLeftX = coordinates[0];
+            int topLeftY = coordinates[1];
+            int bottomRightX = coordinates[2];
+            int bottomRightY = coordinates[3];
+
+            Point topLeft = new Point(topLeftX, topLeftY);
+            Point bottomRight = new Point(bottomRightX, bottomRightY);
+
+            Rectangle rectangle = new Rectangle(topLeft, bottomRight);
+
+            int inputLines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < inputLines; i++)
+            {
+                int[] currentPointCoordinates = Console.ReadLine()
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                int x = currentPointCoordinates[0];
+                int y = currentPointCoordinates[1];
+
+                Point currentPoint = new Point(x, y);
+
+                Console.WriteLine(rectangle.Contains(currentPoint)); 
+            }
+        }
+    }
+}
---
+++ 1. Working with Abstraction Lab/2. Point in Rectangle/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿namespace PointInRectangle
+{
+    public class Rectangle
+    {
+        public Rectangle(Point topLeft, Point bottomRight)
+        {
+            this.TopLeft = topLeft;
+            this.BottomRight = bottomRight;
+        }
+
+        public Point TopLeft { get; private set; }
+        public Point BottomRight { get; private set; }
+
+        public bool Contains(Point point)
+        {
+            if (this.TopLeft.X <= point.X
+                && this.BottomRight.X >= point.X
+                && this.TopLeft.Y <= point.Y
+                && this.BottomRight.Y >= point.Y)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
