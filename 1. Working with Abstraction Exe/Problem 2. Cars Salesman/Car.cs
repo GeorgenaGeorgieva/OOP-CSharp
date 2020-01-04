5941a82f6e0418ac133da032d0053879b160c448
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-04 15:01:49 +0200

    CSharp Solution

---
+++ 1. Working with Abstraction Exe/Problem 2. Cars Salesman/Car.cs
@@ -0,0 +1,71 @@
+﻿namespace CarsSalesman
+{
+    using System.Text;
+    public class Car
+    {
+        private const string offset = "  "; 
+
+        private string model;
+        private Engine engine;
+        private int? weight;
+        private string color;
+
+        public Car(string model, Engine engine)
+        {
+            this.Model = model;
+            this.Engine = engine;
+        }
+
+        public Car(string model, Engine engine, int? weight)
+            : this(model, engine)
+        {
+            this.Weight = (int)weight;
+        }
+
+        public Car(string model, Engine engine, string color)
+            : this (model, engine)
+        {
+            this.Color = color;
+        }
+
+        public Car(string model, Engine engine, int? weight, string color)
+            : this(model, engine)
+        {
+            this.Weight = (int)weight;
+            this.Color = color;
+        }
+
+        public string Model 
+        {
+            get { return this.model; }
+            private set { this.model = value; } 
+        }
+        public Engine Engine 
+        {
+            get { return this.engine; }
+            private set { this.engine = value; } 
+        }
+        public int? Weight 
+        {
+            get { return this.weight; }
+            private set { this.weight = value; } 
+        }
+        public string Color 
+        {
+            get { return this.color; }
+            private set { this.color = value; }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append($"{this.Model}:\n");
+            sb.Append(this.Engine);
+            sb.AppendLine($"{offset}Weight: {(this.Weight == null ? "n/a" : this.Weight.ToString())}");
+            sb.Append($"{offset}Color: {(this.Color == null ? "n/a" : this.Color.ToString())}");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ 1. Working with Abstraction Exe/Problem 2. Cars Salesman/Engine.cs
@@ -0,0 +1,74 @@
+﻿namespace CarsSalesman
+{
+    using System.Text;
+    public class Engine
+    {
+        private const string offset = "  ";
+
+        private string model;
+        private int power;
+        private int? displacement;
+        private string efficiency;
+
+        public Engine(string model, int power)
+        {
+            this.Model = model;
+            this.Power = power;
+        }
+
+        public Engine(string model, int power, int? displacement)
+            :this(model, power)
+        {
+            this.Displacement = (int)displacement;
+        }
+
+        public Engine(string model, int power, string efficiency)
+            :this(model, power)
+        {
+            this.Efficiency = efficiency;
+        }
+
+        public Engine(string model, int power, int? displacement, string efficiency)
+            :this(model, power)
+        {
+            this.Displacement = (int)displacement;
+            this.Efficiency = efficiency;
+        }
+
+        public string Model 
+        {
+            get { return this.model; }
+            private set { this.model = value; } 
+        }
+
+        public int Power 
+        {
+            get { return this.power; }
+            private set { this.power = value; }
+        }
+
+        public int? Displacement
+        {
+            get { return this.displacement; }
+            private set { this.displacement = value; }
+        }
+
+        public string Efficiency 
+        {
+            get { return this.efficiency; }
+            private set { this.efficiency = value; } 
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{offset}{this.Model}:");
+            sb.AppendLine($"{offset}{offset}Power: {this.Power}");
+            sb.AppendLine($"{offset}{offset}Displacement: {(this.Displacement == null ? "n/a" : this.Displacement.ToString())}");
+            sb.AppendLine($"{offset}{offset}Efficiency: {(this.Efficiency == null ? "n/a" : this.Efficiency.ToString())}");
+
+            return sb.ToString();
+        }
+    }
+}
