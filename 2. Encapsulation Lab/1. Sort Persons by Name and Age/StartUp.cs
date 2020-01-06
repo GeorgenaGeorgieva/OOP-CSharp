6157194360d15d201605d72d573c920fddd52ac0
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-06 20:14:24 +0200

    CSharp

---
+++ 2. Encapsulation Lab/1. Sort Persons by Name and Age/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace PersonsInfo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var people = new List<Person>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                var personInformation = Console.ReadLine()
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                var firstName = personInformation[0];
+                var lastName = personInformation[1];
+                var age = int.Parse(personInformation[2]);
+
+                var person = new Person(firstName, lastName, age);
+                people.Add(person);
+            }
+
+            people.OrderBy(p => p.FirstName)
+                .ThenBy(p => p.Age)
+                .ToList()
+                .ForEach(p => Console.WriteLine(p.ToString()));
+        }
+    }
+}
