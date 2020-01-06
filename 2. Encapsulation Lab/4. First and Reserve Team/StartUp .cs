c9e4faff81897aee1a383bb6cb23b3cd11aada9e
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-06 20:17:05 +0200

    CSharp

---
+++ 2. Encapsulation Lab/4. First and Reserve Team/StartUp .cs
@@ -0,0 +1,28 @@
+﻿namespace PersonsInfo
+{
+    using System;
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var team = new Team("The Lions");
+
+            for (int i = 0; i < lines; i++)
+            {
+                var personInformation = Console.ReadLine()
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                var firstName = personInformation[0];
+                var lastName = personInformation[1];
+                var age = int.Parse(personInformation[2]);
+                var salary = decimal.Parse(personInformation[3]);
+
+                var person = new Person(firstName, lastName, age, salary);
+                team.AddPlayer(person);
+            }
+
+            Console.WriteLine(team);
+        }
+    }
+}
