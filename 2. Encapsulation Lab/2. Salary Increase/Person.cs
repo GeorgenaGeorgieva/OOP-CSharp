17458773be9af3d3513727350ae4a7ba7418d01c
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-06 20:15:18 +0200

    CSharp

---
+++ 2. Encapsulation Lab/2. Salary Increase/Person.cs
@@ -0,0 +1,84 @@
+﻿namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+            private set
+            {
+                this.firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            private set
+            {
+                this.lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            private set
+            {
+                this.age = value;
+            }
+        }
+
+        public decimal Salary
+        {
+            get
+            {
+                return this.salary;
+            }
+            private set
+            {
+                this.salary = value;
+            }
+        }
+
+        public void IncreaseSalary(decimal bonus)
+        {
+            if (this.Age <= 30)
+            {
+                bonus /= 2;
+                this.Salary += this.Salary * bonus / 100;
+            }
+            else
+            {
+                this.Salary += this.Salary * bonus / 100;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {this.Salary:F2} leva.";
+        }
+    }
+}
---
+++ 2. Encapsulation Lab/2. Salary Increase/StartUp.cs
@@ -0,0 +1,32 @@
+﻿namespace PersonsInfo
+{
+    using System;
+    using System.Collections.Generic;
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var people = new List<Person>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                var personInformation = Console.ReadLine()
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                var firstName = personInformation[0];
+                var lastName = personInformation[1];
+                var age = int.Parse(personInformation[2]);
+                var salary = decimal.Parse(personInformation[3]);
+
+                var person = new Person(firstName, lastName, age, salary);
+                people.Add(person);
+            }
+
+            var bonus = decimal.Parse(Console.ReadLine());
+
+            people.ForEach(p => p.IncreaseSalary(bonus));
+            people.ForEach(p => Console.WriteLine(p.ToString()));
+        }
+    }
+}
