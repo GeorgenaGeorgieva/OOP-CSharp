a550197ecff5d64c45829d361741387e89243994
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-09 13:34:39 +0200

    CSharp

---
+++ 3. Inheritance Lab/4. Random List/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace CustomRandomList
+{
+    using System;
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var randomList = new RandomList();
+
+            randomList.Add("Ivan");
+            randomList.Add("Petar");
+            randomList.Add("Stafan");
+
+            Console.WriteLine(randomList.RandomString());
+        }
+    }
+}
