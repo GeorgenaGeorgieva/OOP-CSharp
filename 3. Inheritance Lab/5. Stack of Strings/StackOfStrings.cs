871c84aface92c08476b34c80e68a414e6bf8c97
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-09 13:35:06 +0200

    CSharp

---
+++ 3. Inheritance Lab/5. Stack of Strings/StackOfStrings.cs
@@ -0,0 +1,26 @@
+﻿namespace CustomStack
+{
+    using System.Collections.Generic;
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            if (this.Count == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public Stack<string> AddRange(IEnumerable<string> collection)
+        {
+            foreach (var element in collection)
+            {
+                this.Push(element);
+            }
+
+            return this;
+        }
+    }
+}
---
+++ 3. Inheritance Lab/5. Stack of Strings/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace CustomStack
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var customStack = new StackOfStrings();
+
+            Console.WriteLine(customStack.IsEmpty());
+
+            var rangeElements = new Stack<string>();
+            rangeElements.Push("Pesho");
+            rangeElements.Push("Gosho");
+            rangeElements.Push("Ivan");
+
+            customStack.AddRange(rangeElements);
+
+            Console.WriteLine(customStack.IsEmpty());
+
+            foreach (var element in customStack)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
