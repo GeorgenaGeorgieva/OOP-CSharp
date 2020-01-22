0e7d1826577ce3dc7fc208b1ea603b97455f2551
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-22 19:47:37 +0200

    CSharp

---
+++ 3. Inheritance Exe/Problem 1. Person/Person.cs
@@ -0,0 +1,39 @@
+﻿namespace ProblemPerson
+{
+    using System.Text;
+    public class Person
+    {
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Age = age;
+            this.Name = name;
+        }
+
+        public string Name { get; set; }
+
+        public virtual int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            protected set
+            {
+                if (value >= 0)
+                {
+                    this.age = value;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringbuilder = new StringBuilder();
+            stringbuilder.Append($"Name: {this.Name}, Age: {this.Age}");
+
+            return stringbuilder.ToString();
+        }
+    }
+}
---
+++ 3. Inheritance Exe/Problem 1. Person/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace ProblemPerson
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            if (age <= 15)
+            {
+                Child child = new Child(name, age);
+                Console.WriteLine(child);
+            }
+            else if (age > 15)
+            {
+                Person person = new Person(name, age);
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
