68f5d973c61a7e10c85ebe473ec60fb46d9f6560
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-23 22:11:42 +0200

    CSharp

---
+++ 04. Interfaces and Abstraction Exe/Problem 2. Multiple Implementation/Citizen.cs
@@ -0,0 +1,86 @@
+﻿namespace PersonInfo
+{
+    using System;
+    using System.Text;
+
+    public class Citizen : IPerson, IIdentifiable, IBirthable
+    {
+        private string name;
+        private int age;
+        private string id;
+        private string birthdate;
+
+        public Citizen(string name, int age, string id, string birthdate)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Id = id;
+            this.Birthdate = birthdate;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            private set
+            {
+                if (String.IsNullOrEmpty(value) || String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Invalid name!");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Age should be positive number!");
+                }
+
+                this.age = value;
+            }
+        }
+
+        public string Id
+        {
+            get { return this.id; }
+            private set
+            {
+                if (String.IsNullOrEmpty(value) || String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Invalid ID!");
+                }
+
+                this.id = value;
+            }
+        }
+
+        public string Birthdate
+        {
+            get { return this.birthdate; }
+            private set
+            {
+                if (String.IsNullOrEmpty(value) || String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Birthdate is missing!");
+                }
+
+                this.birthdate = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendLine($"{this.Id}");
+            stringBuilder.Append($"{this.Birthdate}");
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 04. Interfaces and Abstraction Exe/Problem 2. Multiple Implementation/StartUp.cs
@@ -0,0 +1,25 @@
+﻿namespace PersonInfo
+{
+	using System;
+
+	public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+			try
+			{
+				var name = Console.ReadLine();
+				var age = int.Parse(Console.ReadLine());
+				var id = Console.ReadLine();
+				var birthdate = Console.ReadLine();
+
+				var citizen = new Citizen(name, age, id, birthdate);
+				Console.WriteLine(citizen);
+			}
+			catch (ArgumentException exception)
+			{
+				Console.WriteLine(exception.Message);	
+			}
+        }
+    }
+}
