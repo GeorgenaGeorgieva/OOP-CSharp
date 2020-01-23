e248c4ed3d9df601df644a0cfbee229999eb36cd
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-23 22:19:12 +0200

    CSharp

---
+++ 05. Polymorphism Lab/1. MathOperation/IMathOperations.cs
@@ -0,0 +1,9 @@
+﻿namespace Operations
+{
+    public interface IMathOperations
+    {
+        int Add(int a, int b);
+        double Add(double a, double b, double c);
+        decimal Add(decimal a, decimal b, decimal c);
+    }
+}
