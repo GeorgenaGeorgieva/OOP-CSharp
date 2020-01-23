cd6a4b4498bcc4fb0ceb0ac69d214440dab9b996
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-23 22:11:06 +0200

    CSharp

---
+++ 04. Interfaces and Abstraction Exe/Problem 1. Define an Interface IPerson/Citizen.cs
@@ -0,0 +1,54 @@
+﻿namespace PersonInfo
+{
+    using System;
+    using System.Text;
+
+    public class Citizen : IPerson
+    {
+        private string name;
+        private int age;
+
+        public Citizen(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            private set
+            {
+                if (String.IsNullOrEmpty(value) || String.IsNullOrWhiteSpace(value))
+                {
+                    throw new AggregateException("Invalid name!");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Age should be positive number!");
+                }
+
+                this.age = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendLine($"{this.Name}");
+            stringBuilder.Append($"{this.Age}");
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 04. Interfaces and Abstraction Exe/Problem 1. Define an Interface IPerson/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace PersonInfo
+{
+	using System;
+
+	public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+			try
+			{
+				string name = Console.ReadLine();
+				int age = int.Parse(Console.ReadLine());
+
+				IPerson person = new Citizen(name, age);
+				Console.WriteLine(person);
+			}
+			catch (ArgumentException exception)
+			{
+				Console.WriteLine(exception.Message);
+				return;
+			}
+        }
+    }
+}
