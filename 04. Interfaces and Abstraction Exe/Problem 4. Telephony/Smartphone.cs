994485a7f1e10a0aeb4e72ce80a386bc9c1bba61
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-23 22:12:19 +0200

    CSharp

---
+++ 04. Interfaces and Abstraction Exe/Problem 4. Telephony/Smartphone.cs
@@ -0,0 +1,43 @@
+﻿namespace Telephony
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Smartphone : IBrowser, ICall
+    {
+        public string Browse(string[] urls)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (var url in urls)
+            {
+                if (url.Any(char.IsDigit))
+                {
+                    stringBuilder.AppendLine("Invalid URL!");
+                    continue;
+                }
+
+                stringBuilder.AppendLine($"Browsing: { url}!");
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+
+        public string Call(string[] phoneNumber)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (var number in phoneNumber)
+            {
+                if (!number.All(char.IsDigit))
+                {
+                    stringBuilder.AppendLine("Invalid number!");
+                    continue;
+                }
+
+                stringBuilder.AppendLine($"Calling... {number}");
+            }
+
+            return stringBuilder.ToString().TrimEnd();  
+        }
+    }
+}
---
+++ 04. Interfaces and Abstraction Exe/Problem 4. Telephony/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace Telephony
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var phoneNumbers = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var URLs = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            Smartphone phone = new Smartphone();
+
+            Console.WriteLine(phone.Call(phoneNumbers));
+            Console.WriteLine(phone.Browse(URLs)); 
+        }
+    }
+}
