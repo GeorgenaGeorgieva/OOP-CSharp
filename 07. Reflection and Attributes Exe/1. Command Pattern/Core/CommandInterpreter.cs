273879f476da12ae79eb116c946e17b6b9a2255d
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-26 22:49:37 +0200

    CSharp

---
+++ 07. Reflection and Attributes Exe/1. Command Pattern/Core/CommandInterpreter.cs
@@ -0,0 +1,40 @@
+﻿using CommandPattern.Core.Commands;
+using CommandPattern.Core.Contracts;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace CommandPattern.Core
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] inputArgs = args
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] commandArgs = inputArgs.Skip(1).ToArray();
+
+            string commandName = (inputArgs[0] + "Command").ToLower();
+
+            Type commandType = Assembly.GetCallingAssembly()
+                .GetTypes()
+                .FirstOrDefault(x => x.Name.ToLower() == commandName);
+
+            if (commandType is null)
+            {
+                throw new ArgumentException("Invalid command type!");
+            }
+
+            ICommand instanceType = Activator.CreateInstance(commandType) as ICommand;
+
+            if (instanceType is null)
+            {
+                throw new ArgumentException("Invalid command type!");
+            }
+           
+            string result = instanceType.Execute(commandArgs);
+
+            return result;
+        }
+    }
+}
---
+++ 07. Reflection and Attributes Exe/1. Command Pattern/Core/Commands/HelloCommand.cs
@@ -0,0 +1,14 @@
+﻿using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Core.Commands
+{
+    public class HelloCommand : ICommand
+    {
+        public string Execute(string[] args)
+        {
+            string name = args[0];
+
+            return $"Hello, {name}";
+        }
+    }
+}
