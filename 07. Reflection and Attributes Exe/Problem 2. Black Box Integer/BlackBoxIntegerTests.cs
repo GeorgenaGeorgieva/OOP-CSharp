cbb33ef3c32a5f7902663a6db0d381d677f3a18d
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-26 22:51:12 +0200

    CSharp

---
+++ 07. Reflection and Attributes Exe/Problem 2. Black Box Integer/BlackBoxIntegerTests.cs
@@ -0,0 +1,47 @@
+﻿namespace P02_BlackBoxInteger
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class BlackBoxIntegerTests
+    {
+        public static void Main()
+        {
+            Type classType = Assembly
+                    .GetCallingAssembly()
+                    .GetTypes()
+                    .FirstOrDefault(x => x.Name is nameof(BlackBoxInteger));
+
+            BlackBoxInteger instance = (BlackBoxInteger)Activator
+                .CreateInstance(classType, true);
+
+            string inputLine = Console.ReadLine();
+
+            while (inputLine != "END")
+            {
+                string[] inputArguments = inputLine
+                    .Split(new[] { '_' }, StringSplitOptions.RemoveEmptyEntries);
+
+                string methodName = inputArguments[0];
+                int number = int.Parse(inputArguments[1]);
+
+                classType.GetMethod(methodName, 
+                    BindingFlags.Instance | 
+                    BindingFlags.NonPublic | 
+                    BindingFlags.Public)
+                    .Invoke(instance, new object[] { number });
+
+                int currentResult = (int)classType.GetField("innerValue",
+                    BindingFlags.Instance |
+                    BindingFlags.NonPublic |
+                    BindingFlags.Public)
+                    .GetValue(instance);
+
+                Console.WriteLine(currentResult);
+
+                inputLine = Console.ReadLine();
+            }
+        }
+    }
+}
