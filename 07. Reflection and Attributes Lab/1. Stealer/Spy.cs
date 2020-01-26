bd97564d6c605ec9d8e0e7d604e5e6b6b7c0a1b6
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-26 22:45:31 +0200

    CSharp

---
+++ 07. Reflection and Attributes Lab/1. Stealer/Spy.cs
@@ -0,0 +1,41 @@
+﻿namespace Stealer
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+
+    public class Spy
+    {
+        public string StealFieldInfo(string investigatedClass, params string[] requestedFields)
+        {
+            Type classType = Assembly.GetCallingAssembly()
+               .GetTypes()
+               .FirstOrDefault(x => x.Name == investigatedClass);
+
+            if (classType is null)
+            {
+                throw new ArgumentException("Invalid class type!");
+            }
+
+            FieldInfo[] classFileds = classType.GetFields(
+                BindingFlags.Instance 
+                | BindingFlags.Static 
+                | BindingFlags.NonPublic 
+                | BindingFlags.Public);
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            Object classInstance = Activator.CreateInstance(classType, new object[] { });
+
+            stringBuilder.AppendLine($"Class under investigation: {investigatedClass}");
+
+            foreach (FieldInfo field in classFileds.Where(f => requestedFields.Contains(f.Name)))
+            {
+                stringBuilder.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
