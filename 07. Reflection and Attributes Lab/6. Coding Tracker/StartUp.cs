acd54435f5114e64e76f8f4261231677400279ef
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-26 22:47:31 +0200

    CSharp

---
+++ 07. Reflection and Attributes Lab/6. Coding Tracker/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CodingTracker
+{
+    [Author("Ventsi")]
+    public class StartUp
+    {
+        [Author("Gosho")]
+        public static void Main(string[] args)
+        {
+            var tracker = new Tracker();
+            var result = tracker.PrintMethodsByAuthor();
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 07. Reflection and Attributes Lab/6. Coding Tracker/Tracker.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace CodingTracker
+{
+    public class Tracker
+    {
+        public string PrintMethodsByAuthor()
+        {
+            var type = typeof(StartUp);
+
+            var methods = type.GetMethods(
+                BindingFlags.Instance |
+                BindingFlags.Public |
+                BindingFlags.Static);
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (var method in methods)
+            {
+                if (method.CustomAttributes.Any(x => x.AttributeType == typeof(AuthorAttribute)))
+                {
+                    var attributes = method.GetCustomAttributes(false);
+                    foreach (AuthorAttribute attr in attributes)
+                    {
+                        stringBuilder.AppendLine($"{method.Name} is written by {attr.Name}");
+                    }
+                }
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
