2df52648e4d40fdf7f28bdf234ffbb65e7219ae9
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-26 22:47:16 +0200

    CSharp

---
+++ 07. Reflection and Attributes Lab/5. Create Attribute/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace CreateAttribute
+{
+    using System;
+
+    [Author("Ventsi")]
+    public class StartUp
+    {
+        [Author("Gosho")]
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
