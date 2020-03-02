5dcff133fcb796fa80db1b0c3b335543c456be9a
Georgena Georgieva <georgena1991@gmail.com>
2020-03-02 19:55:02 +0200

    Update StartUp.cs

--- 04. Interfaces and Abstraction Exe/Problem 1. Define an Interface IPerson/StartUp.cs
+++ 04. Interfaces and Abstraction Exe/Problem 1. Define an Interface IPerson/StartUp.cs
@@ -1,24 +1,24 @@
 ﻿namespace PersonInfo
 {
-	using System;
-
-	public class StartUp
+      using System;
+	  
+     public class StartUp
     {
-        public static void Main(string[] args)
-        {
-			try
-			{
-				string name = Console.ReadLine();
-				int age = int.Parse(Console.ReadLine());
+	  public static void Main(string[] args)
+         {
+           try
+	   {
+               string name = Console.ReadLine();
+	       int age = int.Parse(Console.ReadLine());
 
-				IPerson person = new Citizen(name, age);
-				Console.WriteLine(person);
-			}
-			catch (ArgumentException exception)
-			{
-				Console.WriteLine(exception.Message);
-				return;
-			}
+	       IPerson person = new Citizen(name, age);
+	       Console.WriteLine(person);
+	    }
+	   catch (ArgumentException exception)
+	   {
+	       Console.WriteLine(exception.Message);
+	       return;
+	   }
         }
     }
 }
